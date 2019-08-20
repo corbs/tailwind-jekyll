ca38f9aa73e67ebddb99fa501c6be33e6b68536c
Corbin <corbin.featherstone@gmail.com>
2019-08-20 14:07:48 +1200

    homepage background img

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     colors: {
       primary: '#FEC82A',
       black: '#261E07',
-    }
+    },
+    screens: {
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
   },
   variants: {},
   plugins: []
